27de39b78cd3cebb32d159dae5901e470473e3f4
Olovan <micahsmith.92@gmail.com>
2016-06-10 20:52:56 -0400

    Reverted to original address

--- src/Client.java
+++ src/Client.java
@@ -6,7 +6,7 @@ public class Client
 	public static void main(String[] args)
 	{
 		try {
-			Socket socket = new Socket("172.77.94.206", 9295);
+			Socket socket = new Socket("192.168.254.46", 9295);
 			System.out.println("Client started on port 9295");
 
 			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
